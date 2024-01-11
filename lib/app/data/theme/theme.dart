9b2a0630d04d39e806c077351cdd3cad3e0f10d8
Afiv Dicky Efendy <dickyareda@gmail.com>
2024-01-11 08:38:07 +0700

    refactor: :recycle: rewrite with getx pattern

---
+++ lib/app/data/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle titleText =
+    GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.w500);
+TextStyle subtitleText =
+    GoogleFonts.inconsolata(fontSize: 18, fontWeight: FontWeight.w400);
+TextStyle bodyText =
+    GoogleFonts.inconsolata(fontSize: 14, fontWeight: FontWeight.w500);
+
+Color redColor = const Color(0xffBF3131);
+Color greenColor = const Color(0xff88AB8E);
+Color primaryColor = const Color(0xff756AB6);
+Color secondaryColor = const Color(0xff453e6b);
+Color thirdColor = const Color(0xffFFB100);
