ca155fee5aff6d7af202c2319ddf3c76cddbc715
Afiv Dicky Efendy <dickyareda@gmail.com>
2024-01-11 13:41:48 +0700

    build: :heavy_plus_sign: add and configure image picker

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegisterController extends GetxController {
   final registerKey = GlobalKey<FormState>();
   var nameCtrl = TextEditingController();
   var emailCtrl = TextEditingController();
   var passwordCtrl = TextEditingController();
+  var selectedImg = Rx<XFile?>(null);
+
+  Future<void> takeImgFromGallery() async {
+    selectedImg.value =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+  }
 }
--- lib/app/modules/register/views/register_profile_image_view.dart
+++ lib/app/modules/register/views/register_profile_image_view.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:conveyance_apps/app/data/theme/theme.dart';
+import 'package:conveyance_apps/app/modules/register/controllers/register_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +9,7 @@ class RegisterProfileImageView extends StatelessWidget {
   const RegisterProfileImageView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final registerCtrl = Get.find<RegisterController>();
     return Scaffold(
         appBar: AppBar(
           title: const Text('RegisterProfileImageView'),
@@ -21,15 +25,49 @@ class RegisterProfileImageView extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 40, bottom: 60),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                color: secondaryColor,
-                borderRadius: BorderRadius.circular(60),
-              ),
-            ),
+            Obx(() => GestureDetector(
+                  onTap: () => registerCtrl.takeImgFromGallery(),
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 40, bottom: 60),
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            color: secondaryColor,
+                            borderRadius: BorderRadius.circular(60),
+                            image: registerCtrl.selectedImg.value != null
+                                ? DecorationImage(
+                                    image: FileImage(File(
+                                        registerCtrl.selectedImg.value!.path)),
+                                    fit: BoxFit.cover)
+                                : null),
+                        child: registerCtrl.selectedImg.value == null
+                            ? const Icon(
+                                Icons.person,
+                                color: Colors.white,
+                                size: 80,
+                              )
+                            : null,
+                      ),
+                      Positioned(
+                          bottom: 55,
+                          right: 5,
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                color: whiteColor,
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Icon(
+                              Icons.add,
+                              color: greenColor,
+                              size: 30,
+                            ),
+                          ))
+                    ],
+                  ),
+                )),
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 25),
               width: Get.width,
